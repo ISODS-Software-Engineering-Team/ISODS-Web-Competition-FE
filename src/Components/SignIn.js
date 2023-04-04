206713f64718350d478ca880a505d5fc8ec62764
Long Nguyen <long.nguyen@isods.org>
2023-04-04 10:10:14 -0400

    Deleted unnecessary files, add few dependencies: react-router-dom, also first commit with signin

---
+++ src/Components/SignIn.js
@@ -0,0 +1,14 @@
+
+
+function SignIn() {
+
+
+    return (
+        <div>
+            <h1> World</h1>
+            <button> This is React</button>
+        </div>
+    );
+}
+
+export default SignIn
