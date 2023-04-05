fb0e48f9e6b4c8e51bbf0eddecd1bc3eeee57471
Long Nguyen <long.nguyen@isods.org>
2023-04-05 09:39:42 -0400

    UserSignIn page with button calling BE (unfinished)

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -1,64 +1,99 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import './SignIn.css';
-import { CssBaseline, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import { red, pink } from '@mui/material/colors';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
-function SignIn() {
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-SCSRFToken';
+axios.defaults.withCredentials = true;
+
+// const client = axios.create({
+//   baseURL: "http://127.0.0.1:8000"
+// });
+
+function UserSignIn() {
+
+    let navigate = useNavigate();
+    // const [currentUser, setCurrentUser] = useState();
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+        const data = new FormData(event.currentTarget);
+        const signIn = await axios.post('http://localhost:8000/api/login', {
+            email: data.get('email'),
+            password: data.get('password'),
+        }).then(function (res) {
+            console.log(signIn)
+        });
+
+        if (signIn.data === true) {
+            return navigate("/");
+        } else {
+            alert("wrong password");
+        }
+    }
+
+
 
     return (
         <div>
-            <div class="container">
+            <div className="container">
                 <h1> ISODS </h1>
             </div>
-            <div class='goback-button'>
+            <div className='goback-button'>
                 <Link href="/" variant="body2">
                     « back
                 </Link>
             </div>
-            <div class='container-form'>
-                <Box
+            <div className='container-form'>
+                <Box component='form' onSubmit={handleSubmit}
                     sx={{
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
                     }}>
-                    <div class='sign-in-header'> Sign In </div>
+                    <div className='sign-in-header'> Sign In </div>
                     <TextField
-                        id="outlined-disabled"
+                        id="email"
 
-                        label="Username"
+                        label="Username/email"
                     />
                     <TextField
-                        id="outlined-disabled"
+                        id="password"
                         label="Password"
                     />
+                    <div className='sign-in-button'>
+                        <Button
+                            type="submit"
+                            variant="palette"
+                            sx={{ mt: 3, mb: 2 }}
+                            palette={{
+                                primary: red,
+                                secondary: pink
+                            }}>
+                            Sign In
+                        </Button>
+                    </div>
                 </Box>
-                <div class='sign-in-button'>
-                    <Button
-                        variant="palette"
-                        sx={{ mt: 3, mb: 2 }}
-                        palette={{
-                            primary: red,
-                            secondary: pink
-                        }}>
-                        Sign In
-                    </Button>
+                <div className='forgot-username-password'>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot Username / Password?
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </div>
             </div>
-            <div class='forgot-username-password'>
-                <Grid container>
-                    <Grid item xs>
-                        <Link href="#" variant="body2">
-                            Forgot Username / Password?
-                        </Link>
-                    </Grid>
-                </Grid>
-            </div>
+
         </div>
     );
 }
 
-export default SignIn
+export default UserSignIn
