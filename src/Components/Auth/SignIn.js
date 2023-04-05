2327cab5c59fe6bb14463f27a8981e3f32cd070c
Long Nguyen <long.nguyen@isods.org>
2023-04-05 12:23:10 -0400

    Implemented feature Log-in for user (communication between FE and BE), UI/UX is still needed to be implement.

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -5,94 +5,66 @@ import { Button } from '@mui/material';
 import { red, pink } from '@mui/material/colors';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
-import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import {useState } from 'react'; 
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import { useNavigate } from "react-router-dom";
+
 
 axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-SCSRFToken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
-// const client = axios.create({
-//   baseURL: "http://127.0.0.1:8000"
-// });
+const client = axios.create({
+  baseURL: "http://localhost:8000"
+});
 
 function UserSignIn() {
-
     let navigate = useNavigate();
-    // const [currentUser, setCurrentUser] = useState();
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
+    const [currentUser, setCurrentUser ] = useState();
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
 
-    const handleSubmit = async (event) => {
-        const data = new FormData(event.currentTarget);
-        const signIn = await axios.post('http://localhost:8000/api/login', {
-            email: data.get('email'),
-            password: data.get('password'),
-        }).then(function (res) {
-            console.log(signIn)
+    function submitLogin(e){
+        e.preventDefault();
+        client.post(
+            "/api/login",
+            {
+                email:email,
+                password: password
+            }
+        ).then(function(res){
+            console.log(setCurrentUser);
+            setCurrentUser(true);
         });
-
-        if (signIn.data === true) {
-            return navigate("/");
-        } else {
-            alert("wrong password");
-        }
     }
 
 
-
+    if(currentUser){
+        navigate('/')
+    }
     return (
-        <div>
-            <div className="container">
-                <h1> ISODS </h1>
-            </div>
-            <div className='goback-button'>
-                <Link href="/" variant="body2">
-                    « back
-                </Link>
-            </div>
-            <div className='container-form'>
-                <Box component='form' onSubmit={handleSubmit}
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}>
-                    <div className='sign-in-header'> Sign In </div>
-                    <TextField
-                        id="email"
+        <div className="center">
+        <Form onSubmit={e => submitLogin(e)}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
 
-                        label="Username/email"
-                    />
-                    <TextField
-                        id="password"
-                        label="Password"
-                    />
-                    <div className='sign-in-button'>
-                        <Button
-                            type="submit"
-                            variant="palette"
-                            sx={{ mt: 3, mb: 2 }}
-                            palette={{
-                                primary: red,
-                                secondary: pink
-                            }}>
-                            Sign In
-                        </Button>
-                    </div>
-                </Box>
-                <div className='forgot-username-password'>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot Username / Password?
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </div>
-            </div>
-
-        </div>
     );
 }
 
