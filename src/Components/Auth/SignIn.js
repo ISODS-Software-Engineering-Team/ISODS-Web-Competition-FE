0cf9228939ca372d1541f88777ea7a0b827f1bd2
Long Nguyen <long.nguyen@isods.org>
2023-04-05 14:12:39 -0400

    Changed UI, need to implement JWT

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -1,70 +1,95 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import './SignIn.css';
 import { Button } from '@mui/material';
-import { red, pink } from '@mui/material/colors';
+import axios from 'axios';
+import { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
-import axios from 'axios';
-import {useState } from 'react'; 
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import { useNavigate } from "react-router-dom";
-
-
+import TextField from '@mui/material/TextField';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: "http://localhost:8000"
+    baseURL: "http://localhost:8000"
 });
 
 function UserSignIn() {
     let navigate = useNavigate();
-    const [currentUser, setCurrentUser ] = useState();
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
+    const [currentUser, setCurrentUser] = useState();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    function submitLogin(e){
+    function submitLogin(e) {
         e.preventDefault();
-        client.post(
+        
+        const response = client.post(
             "/api/login",
             {
-                email:email,
+                email: email,
                 password: password
             }
-        ).then(function(res){
-            console.log(setCurrentUser);
+        ).then(function (res) {
             setCurrentUser(true);
         });
     }
 
 
-    if(currentUser){
+    if (currentUser) {
         navigate('/')
     }
     return (
-        <div className="center">
-        <Form onSubmit={e => submitLogin(e)}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+        <div>
+            <div className="container">
+                <h1> ISODS </h1>
+            </div>
 
+            <div className="container">
+                <div className="goback-button">
+                    <a href="/"> « back</a>
+                </div>
+                <div className="container-form">
+                    <div className="sign-in-header">
+                        Sign In
+                    </div>
+                    <Navbar bg="dark" variant="dark">
+                        <Form onSubmit={e => submitLogin(e)}>
+                            <div className="email-field">
+                                <TextField
+                                    id="email"
+                                    label="Username/email"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <TextField
+                                id="password"
+                                label="Password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                type="password"
+                            />
+                            <div className='sign-in-button'>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </div>
+                            <div className='forgot-username-password'>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link href="#" variant="body2">
+                                            Forgot Username / Password?
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </Form>
+                    </Navbar>
+                </div>
+            </div >
+        </div >
     );
 }
 
