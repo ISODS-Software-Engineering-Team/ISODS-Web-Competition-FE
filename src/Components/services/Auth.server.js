401768a061a2b7009d9295c06f6afcbc10e9116a
Emmy Tran <emmy.tran@isods.org>
2023-04-11 10:08:03 -0700

    test

--- src/Components/services/Auth.server.js
+++ src/Components/services/Auth.server.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
+
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const API_URL_USERS = "http://localhost:8000/auth/users";
-const API_URL_LOGIN =  "http://localhost:8000/auth/jwt"
+const API_URL_LOGIN =  "http://localhost:8000/auth/jwt";
 
-// const login takes 2 arguments: email and password.
-// using axios for communication between FE and BE
-const login = (email, password) =>{
-    return axios.post(API_URL_LOGIN + "/create", {
+const login = (email, password) => {
+    return axios.post(`${API_URL_LOGIN}/create`, {
         email,
         password,
     }).then(response => {
-        if(response.data.access_token){ // this will return access_token (response from BE)
-            // using local Storage to store info of user such as access_token as value and key as "user"
+        if (response.data.access_token) {
             localStorage.setItem("User", JSON.stringify(response.data.access_token));
         }
         return response.data;
@@ -22,10 +20,14 @@ const login = (email, password) =>{
 }
 
 const requestEmail = (email) => {
-    return axios.get(API_URL_USERS + "/reset_password/",
-    {
-        email: email,
-    })
+    return axios.get(`${API_URL_USERS}/reset_password/?email=${email}`)
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            throw error;
+        });
 }
 
 const logout = () => {
