aa725a51d4a4323f466a887c43b4d7f89909726c
Anh Pham <anh.pham@isods.org>
2023-04-15 12:10:23 -0500

    add js file and css file for competition page (not finished yet)

---
+++ src/Components/Competition/CompetitionPage.js
@@ -0,0 +1,50 @@
+
+
+function CompetitionPage() {
+
+
+<body>
+    <div class="search-container">
+        <form action="" class="search-bar">
+            <input type="text" placeholder="Search bar" aria-label="search"/>
+            <button>
+                <i class="fa-solid fa-magnifying-glass" class="search-bar__submit" aria-label="submit search"></i>            
+            </button>
+        </form>
+    </div>
+
+    <div class="create-competition-btn">
+        <button type="button"> Host a Competition </button>
+    </div>
+
+    <div class="search-competition-container">
+        <form action="" class="search-competition-bar"></form>
+            <input type="text" placeholder="Search for Competition">
+    </div>
+
+    
+
+    <div class="header"> 
+        <div class="side-nav">
+            <a href="#" class="logo">
+                <img src="images/logo.png" class="logo-img" alt="">
+                <img src="images/laurel-wreath.png" class="logo-icon" alt="">
+            </a>
+            <ul class="nav-links">
+                <li><a href="#"><i class="fa-solid fa-house"></i><p>Home</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-trophy"></i><p>Competition</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-cubes"></i><p>Models</p></a></li>
+                <li><a href="#"><i class="fa-regular fa-comments"></i><p>Discussion</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-code"></i><p>Code</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-caret-down"></i><p>More</p></a></li>
+                <div class="active"></div>
+            </ul>
+        </div>
+    </div>
+    <div class="competition-container">
+        
+    </div>
+</body>
+
+</html>
+}
