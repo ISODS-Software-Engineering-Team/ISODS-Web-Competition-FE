b3b7ee26ee5c6f38a5a8d7ac21802f972ecff6b6
Emmy Tran <emmy.tran@isods.org>
2023-04-28 09:39:54 -0700

    Sidebar hover

--- src/Components/Competition/Competition.js
+++ src/Components/Competition/Competition.js
@@ -14,21 +14,35 @@ import CompetitionData from "./CompetitionData";
 
 const Competition = ({children}) => {
     const [isOpen ,setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
+
+    const handleMouseEnter = () => {
+        setIsOpen(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsOpen(false);
+    }
 
     return (
         <div className="container">
-            <div style={{width: isOpen ? "200px" : "70px", height: "100%", overflow: "auto"}} className="sidebar">
+            <div style={{width: isOpen ? "200px" : "70px"}} className="sidebar">
                 <div className="top_section">
                     <div style={{marginLeft: isOpen ? "8px" : "8px"}} className="bars">
                         <div className="bars-bg" />
-                        <FaBars onClick={toggle}/>
+                        <FaBars onClick={() => setIsOpen(!isOpen)} />
                     </div>
                     <h1 style={{display: isOpen ? "block" : "none", "color" : "orange", "padding-left": "20px"}} className="logo">ISODS</h1>
 
                 </div>
                 {CompetitionData.map((item, index) => (
-                    <NavLink to={item.path} key={index} className="link" activeClassName="active">
+                    <NavLink
+                        to={item.path}
+                        key={index}
+                        className="link"
+                        activeClassName="active"
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
                         <div className="icon">{item.icon}</div>
                         <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
                     </NavLink>
