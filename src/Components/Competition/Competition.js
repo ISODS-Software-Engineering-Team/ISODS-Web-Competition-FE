50481c8793e455967658616895277e6cabe3a5d2
Emmy Tran <emmy.tran@isods.org>
2023-04-28 13:48:06 -0700

    update search bar

--- src/Components/Competition/Competition.js
+++ src/Components/Competition/Competition.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from '../Utils/NavBar';
 import '../Utils/NavBar.css';
 import NavBarData from '../Utils/NavBarData';
 import './Competition.css';
 
-const Competition = ({ children }) => {
+const SearchBar = () => {
   return (
-    <div>
-      <div className="comp-header">
-        <NavBar data={NavBarData} />
-        <div className="comp-auth-buttons-container">
-          <button className="comp-login-button">Sign In</button>
-          <button className="comp-signup-button">Register</button>
-        </div>
-      </div>
-      <main>{children}</main>
+    <div className="search-bar">
+      <input type="text" placeholder="Search..." />
     </div>
   );
 };
+
+const Competition = ({ children }) => {
+    return (
+      <div>
+        <NavBar data={NavBarData} />
+        <div className="search-bar-container">
+          <SearchBar />
+        </div>
+        <div className="comp-info">
+          <h1>Competition</h1>
+        </div>
+        <div className="comp-auth-buttons-container">
+          <Link to="/signin">
+            <button className="comp-login-button">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="comp-signup-button">Register</button>
+          </Link>
+        </div>
+        <main>{children}</main>
+      </div>
+    );
+  };
+  
 
 export default Competition;
