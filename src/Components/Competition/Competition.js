e89817446d7af4a0b2e2af46d34968d9845a9c2e
Emmy Tran <emmy.tran@isods.org>
2023-04-28 16:13:37 -0700

    init

--- src/Components/Competition/Competition.js
+++ src/Components/Competition/Competition.js
@@ -5,47 +5,90 @@ import '../Utils/NavBar.css';
 import NavBarData from '../Utils/NavBarData';
 import './Competition.css';
 import { FaSearch } from 'react-icons/fa';
+import CompetitionData from './CompetitionData';
 
 const SearchBar = () => {
-    return (
-        <div className="search-bar">
-            <input type="text" placeholder="Search" />
-            <div className="search-icon">
-                <FaSearch />
-            </div>
-        </div>
-    );
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search" />
+      <div className="search-icon">
+        <FaSearch />
+      </div>
+    </div>
+  );
+};
+
+const CompetitionSearch = () => {
+  return (
+    <div className="competition-search">
+      <input type="text" placeholder="Search competition" />
+      <div className="search-icon">
+        <FaSearch />
+      </div>
+    </div>
+  );
+};
+
+const CompetitionCard = ({ competition }) => {
+  return (
+    <div className="card">
+      <img src={competition.imageUrl} alt={competition.name} />
+      <h3>{competition.name}</h3>
+      <p>{competition.info}</p>
+      <Link to={`/competitions/${competition.id}`} className="card-button">
+        View Details
+      </Link>
+    </div>
+  );
 };
 
 const Competition = ({ children }) => {
     return (
-        <div className="comp-container">
-            <NavBar data={NavBarData} />
-
-            {/* Search bar */}
-            <div className="search-bar-container">
-                <SearchBar />
-            </div>
-
-            {/* Sign in and Register buttons */}
-            <div className="comp-auth-buttons-container">
-                <Link to="/signin">
-                    <button className="comp-login-button">Sign In</button>
-                </Link>
-                <Link to="/signup">
-                    <button className="comp-signup-button">Register</button>
-                </Link>
-            </div>
-
-            {/* Competition Info */}
-            <div className="comp-info">
-                <h1>Competition</h1>
-                <p className="comp-paragraph">Grow your data science skills by competing in our exciting competitions. Find help in the documentation or learn about Community Competitions.</p>
-            </div>
-
-            <main className="comp-main">{children}</main>
+      <div className="comp-container">
+        <NavBar data={NavBarData} />
+  
+        {/* Search bar and authentication buttons */}
+        <div className="comp-header">
+          <div className="search-bar-container">
+            <SearchBar />
+          </div>
+          <div className="comp-auth-buttons-container">
+            <Link to="/signin">
+              <button className="comp-login-button">Sign In</button>
+            </Link>
+            <Link to="/signup">
+              <button className="comp-signup-button">Register</button>
+            </Link>
+          </div>
         </div>
+  
+        {/* Competition Info */}
+        <div className="comp-info">
+          <h1>Competition</h1>
+          <p className="comp-paragraph">
+            Grow your data science skills by competing in our exciting
+            competitions. Find help in the documentation or learn about
+            Community Competitions.
+          </p>
+        </div>
+  
+        {/* Competition Search */}
+        <CompetitionSearch />
+  
+        {/* Competition Cards */}
+        <div className="card-container">
+          {CompetitionData.slice(0, 9).map((competition) => (
+            <CompetitionCard key={competition.id} competition={competition} />
+          ))}
+        </div>
+  
+        <main className="comp-main">{children}</main>
+      </div>
     );
-};
-
-export default Competition;
+  };
+  
+  export default Competition;
+  
+  
+  
+  
---
+++ src/Components/Competition/CompetitionData.js
@@ -0,0 +1,58 @@
+const CompetitionData = [
+  {
+    id: 1,
+    name: 'Competition 1',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 1'
+  },
+  {
+    id: 2,
+    name: 'Competition 2',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 2'
+  },
+  {
+    id: 3,
+    name: 'Competition 3',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 3'
+  },
+  {
+    id: 4,
+    name: 'Competition 4',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 4'
+  },
+  {
+    id: 5,
+    name: 'Competition 5',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 5'
+  },
+  {
+    id: 6,
+    name: 'Competition 6',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 6'
+  },
+  {
+    id: 7,
+    name: 'Competition 7',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 7'
+  },
+  {
+    id: 8,
+    name: 'Competition 8',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 8'
+  },
+  {
+    id: 9,
+    name: 'Competition 9',
+    imageUrl: 'https://www.isods.org//images/isods_small_logo.png',
+    info: 'Some information about competition 9'
+  }
+];
+
+export default CompetitionData;
